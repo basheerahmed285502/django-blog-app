775c3fcbdcd01a75ecf1e6ea5c986ced86a6071e
Abu Muhammed <basheerahmed285502@gmail.com>
2026-08-04 18:36:47 +0400

    Fix the staticfiles.E005 system check error

--- blog/settings.py
+++ blog/settings.py
@@ -124,5 +124,8 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
-    }
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
 }
