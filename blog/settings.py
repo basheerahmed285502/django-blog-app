f399bc398b2a4d50873609d869ee1a7d97747829
Abu Muhammed <basheerahmed285502@gmail.com>
2026-08-04 17:15:47 +0400

    Update settings.py allowedhosts

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w*s3!lp)ago-c*kb%&f9u&ud_=$20crtk_on3oxmyhy!d-qo2m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
